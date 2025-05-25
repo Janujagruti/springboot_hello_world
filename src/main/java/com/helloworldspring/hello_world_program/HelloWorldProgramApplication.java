de065d8d06d2f75ebe35155e0a62a11d2cb477c7
Janujagruti <ranejanhavi1@gmail.com>
2025-05-25 14:00:21 +0530

    Initial commit of hello_world_program

---
+++ src/main/java/com/helloworldspring/hello_world_program/HelloWorldProgramApplication.java
@@ -0,0 +1,13 @@
+package com.helloworldspring.hello_world_program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helloworldspring/hello_world_program/HelloWorldProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworldspring.hello_world_program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
